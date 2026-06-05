87a87e7ac6adce9a5e923a6f2b4718490f9487a0
islahuddin <33101046+islahuddinafandi@users.noreply.github.com>
2026-06-05 09:17:25 +0700

    change

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = String(import.meta.env.'https://ltjbewwrweirtaxzefvx.supabase.co')
-const supabaseAnonKey = String(import.meta.env.'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0amJld3dyd2VpcnRheHplZnZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2MDU2MzMsImV4cCI6MjA5NjE4MTYzM30.pcmcVR2_siZdMLtmg_iIwEKxTiQvn-8TSVCe3R6iTd4')
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  'https://ltjbewwrweirtaxzefvx.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0amJld3dyd2VpcnRheHplZnZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2MDU2MzMsImV4cCI6MjA5NjE4MTYzM30.pcmcVR2_siZdMLtmg_iIwEKxTiQvn-8TSVCe3R6iTd4'
+)
